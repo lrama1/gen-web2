3512503312e8dadaa87724e130bcad51864cba64
Lawrence Rama <oleng_rama@hotmail.com>
2022-04-01 20:22:18 -0400

    temp commit

--- gen-web2/src/main/resources/data.sql
+++ gen-web2/src/main/resources/data.sql
@@ -11,48 +11,41 @@ VALUES
 ('Sample-personId1','Sample-firstName1','Sample-lastName1')
 ;
 
+INSERT INTO ITEM_ATTR_TYPE_DATATYPE(
+  ITEM_ATTR_TYPE_DATATYPE_ID,
+  ITEM_ATTR_TYPE_DATATYPE_NAME,
+  ITEM_ATTR_TYPE_DATATYPE_DESC,
+  REGEX_PATTERN
+)
+VALUES
+(1, 'STRING','STRING', ''),
+(2, 'NUMBER','NUMBER', '')
+;
+
+
 INSERT INTO ITEM_TYPE(ITEM_TYPE_ID, ITEM_TYPE_CODE, ITEM_TYPE_NAME, ITEM_TYPE_DESC)
 VALUES
 ('001','PERSON', 'Person', 'Person Entity'),
 ('002','COMPANY', 'Company', 'Company Entity');
 
 
-
-INSERT INTO itemType
-(
-item_type_id
-,item_type_code
-,item_type_name
-,item_type_desc
+INSERT INTO ITEM_ATTRIBUTE_TYPE(
+  ITEM_ATTR_TYPE_ID,  ITEM_ATTR_TYPE_CODE,
+  ITEM_ATTR_TYPE_NAME,  ITEM_ATTR_TYPE_DESC,
+  ITEM_ATTR_TYPE_DATATYPE_ID, ITEM_ATTR_TYPE_LENGTH,
+  ITEM_ATTR_TYPE_ISSEARCHABLE,  ITEM_ATTR_TYPE_DISPLAY_INDEX,
+  ITEM_ATTR_TYPE_IS_REQUIRED,  ITEM_ATTR_TYPE_IS_METAPHONEKEY,
+  ITEM_ATTR_TYPE_METAPHONEKEYPOS,  ITEM_ATTR_TYPE_LOOKUP_LIST_ID,
+  ITEM_TYPE_ID
 )
-VALUES
-('Sample-itemTypeId0','Sample-itemTypeCode0','Sample-itemTypeName0','Sample-itemTypeDesc0')
-,
-('Sample-itemTypeId1','Sample-itemTypeCode1','Sample-itemTypeName1','Sample-itemTypeDesc1')
-,
-('Sample-itemTypeId2','Sample-itemTypeCode2','Sample-itemTypeName2','Sample-itemTypeDesc2')
-,
-('Sample-itemTypeId3','Sample-itemTypeCode3','Sample-itemTypeName3','Sample-itemTypeDesc3')
-,
-('Sample-itemTypeId4','Sample-itemTypeCode4','Sample-itemTypeName4','Sample-itemTypeDesc4')
-,
-('Sample-itemTypeId5','Sample-itemTypeCode5','Sample-itemTypeName5','Sample-itemTypeDesc5')
-,
-('Sample-itemTypeId6','Sample-itemTypeCode6','Sample-itemTypeName6','Sample-itemTypeDesc6')
-,
-('Sample-itemTypeId7','Sample-itemTypeCode7','Sample-itemTypeName7','Sample-itemTypeDesc7')
-,
-('Sample-itemTypeId8','Sample-itemTypeCode8','Sample-itemTypeName8','Sample-itemTypeDesc8')
-,
-('Sample-itemTypeId9','Sample-itemTypeCode9','Sample-itemTypeName9','Sample-itemTypeDesc9')
-,
-('Sample-itemTypeId10','Sample-itemTypeCode10','Sample-itemTypeName10','Sample-itemTypeDesc10')
-,
-('Sample-itemTypeId11','Sample-itemTypeCode11','Sample-itemTypeName11','Sample-itemTypeDesc11')
-,
-('Sample-itemTypeId12','Sample-itemTypeCode12','Sample-itemTypeName12','Sample-itemTypeDesc12')
-,
-('Sample-itemTypeId13','Sample-itemTypeCode13','Sample-itemTypeName13','Sample-itemTypeDesc13')
+VALUES(
+  '00001', 'FIRST_NAME', 
+  'First Name', 'First Name',
+  1, 30,
+  1, 1,
+  0, 0,
+  1, NULL,
+  '001'
+)
 ;
-
 
